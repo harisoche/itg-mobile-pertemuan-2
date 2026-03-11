4eaae5185d2da5032ec229b7b8d3fa640bc7a660
Shailanajwa <shaila.najwa99@gmail.com>
2026-03-11 12:02:24 +0700

    add decrement functionality and update info page content

--- lib/pages/info_page.dart
+++ lib/pages/info_page.dart
@@ -13,12 +13,12 @@ class InfoPage extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: const [
             Text(
-              'Ini StatelessWidget',
+              'Shaila Najwa Aristianti',
               style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
             ),
             SizedBox(height: 8),
             Text(
-              'Halaman ini tidak menyimpan state yang berubah.',
+              'Shaila Najwa Aristianti-Nim 2306125',
               style: TextStyle(fontSize: 16),
             ),
           ],
