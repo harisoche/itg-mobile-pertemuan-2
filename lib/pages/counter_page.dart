0a38189b7da49e65310308afee5ab0be762ac53d
SalmaAulia29 <auliasalma650@gmail.com>
2026-03-11 16:15:36 +0700

    TugasPertemuan2

--- lib/pages/counter_page.dart
+++ lib/pages/counter_page.dart
@@ -11,6 +11,7 @@ class _CounterPageState extends State<CounterPage> {
   int count = 0;
 
   void tambah() => setState(() => count++);
+  void kurangi() => setState(() => count = count > 0 ? count - 1 : 0);
   void reset() => setState(() => count = 0);
 
   @override
@@ -32,6 +33,11 @@ class _CounterPageState extends State<CounterPage> {
               mainAxisSize: MainAxisSize.min,
               children: [
                 ElevatedButton(onPressed: tambah, child: const Text('Tambah')),
+                const SizedBox(width: 12),
+                ElevatedButton(
+                  onPressed: count > 0 ? kurangi : null,
+                  child: const Text('Kurangi'),
+                ),
                 const SizedBox(width: 12),
                 OutlinedButton(onPressed: reset, child: const Text('Reset')),
               ],
