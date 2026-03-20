d6e777affa56b4a13b46bd84d5bee019d4693091
kamila <2306098@itg.ac.id>
2026-03-20 10:38:34 +0700

    add decrement button and update info text on CounterPage (#30)

--- lib/pages/info_page.dart
+++ lib/pages/info_page.dart
@@ -13,12 +13,12 @@ class InfoPage extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: const [
             Text(
-              'Ini StatelessWidget',
+              'Kamilaeni',
               style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
             ),
             SizedBox(height: 8),
             Text(
-              'Halaman ini tidak menyimpan state yang berubah.',
+              '2306098',
               style: TextStyle(fontSize: 16),
             ),
           ],
