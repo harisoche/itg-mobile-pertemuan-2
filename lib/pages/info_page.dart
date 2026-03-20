9a615917b75d67a345d902a74c6c21f8b7a6bfa2
Fathir Miftah Nursalim <miftahfathir7@gmail.com>
2026-03-20 10:38:51 +0700

    Tugas Pertemuan 2 (#24)

--- lib/pages/info_page.dart
+++ lib/pages/info_page.dart
@@ -13,12 +13,12 @@ class InfoPage extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: const [
             Text(
-              'Ini StatelessWidget',
+              'Nama : Fathir Miftah Nursalim',
               style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
             ),
             SizedBox(height: 8),
             Text(
-              'Halaman ini tidak menyimpan state yang berubah.',
+              'Nim : 2306135',
               style: TextStyle(fontSize: 16),
             ),
           ],
