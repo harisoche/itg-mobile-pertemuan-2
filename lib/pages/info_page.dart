2cca4f879598d07d34bd12ad2a661b1225f94af6
HanifLMtq <hanluqman8@gmail.com>
2026-03-20 10:39:29 +0700

    Tugas_pertemuan 2 (#9)

--- lib/pages/info_page.dart
+++ lib/pages/info_page.dart
@@ -13,12 +13,12 @@ class InfoPage extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: const [
             Text(
-              'Ini StatelessWidget',
+              '2306100',
               style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
             ),
             SizedBox(height: 8),
             Text(
-              'Halaman ini tidak menyimpan state yang berubah.',
+              '2306100 - Saya Hanif Luqman Muttaqin',
               style: TextStyle(fontSize: 16),
             ),
           ],
