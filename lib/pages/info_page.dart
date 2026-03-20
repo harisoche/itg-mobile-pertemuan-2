defb44aa6ee6f1c16bf5a32f516594ff30458a08
binachuu <tabinar121004@gmail.com>
2026-03-20 10:38:37 +0700

    Saya commit tugas real no fake (#29)

--- lib/pages/info_page.dart
+++ lib/pages/info_page.dart
@@ -6,22 +6,42 @@ class InfoPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Stateless Page')),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: const [
-            Text(
-              'Ini StatelessWidget',
-              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 8),
-            Text(
-              'Halaman ini tidak menyimpan state yang berubah.',
-              style: TextStyle(fontSize: 16),
-            ),
-          ],
+      appBar: AppBar(
+        title: const Text('Stateless Page'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Text(
+                'Ini Iwak Antikarbit',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+
+              const SizedBox(height: 16),
+
+              Image.network(
+                'https://pbs.twimg.com/media/FTc0IE1VUAAXieD.jpg',
+                height: 200,
+                fit: BoxFit.cover,
+              ),
+
+              const SizedBox(height: 16),
+
+              const Text(
+                'Konon katanya.... -Tabina AR (2306110)',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 16),
+              ),
+            ],
+          ),
         ),
       ),
     );
