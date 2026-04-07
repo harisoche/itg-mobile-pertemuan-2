6a0798bfaadff04dede22f8de574b36f8f9058b3
LonelyJyy <fadhliannur1611@gmail.com>
2026-04-07 10:08:54 +0700

    add decrement button and prevent negative count

--- lib/pages/info_page.dart
+++ lib/pages/info_page.dart
@@ -13,12 +13,12 @@ class InfoPage extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: const [
             Text(
-              'Ini StatelessWidget',
+              'Nama: Fadhlian Nur Fajri', // ganti sesuai nama kamu
               style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
             ),
             SizedBox(height: 8),
             Text(
-              'Halaman ini tidak menyimpan state yang berubah.',
+              'NIM: 2306147', // ganti sesuai NIM kamu
               style: TextStyle(fontSize: 16),
             ),
           ],
